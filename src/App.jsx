7335c94920305c6aae253102bb5de0ff415bbf80
IsraelPeled <=>
2024-10-14 21:54:33 +0300

    Adding Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import Card from "./Card";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Card
         image="https://static.wikia.nocookie.net/disney/images/3/31/Profile_-_Baloo.jpeg"
         name="Baloo"
